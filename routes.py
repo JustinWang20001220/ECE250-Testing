3868595b4afd360bedcc098778d66d085fd6b845
zefeiO <ouzefei1551@gmail.com>
2021-11-03 22:09:49 -0400

    some ideas

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 from flask import request, session
+import subprocess
 
 TESTS = ["push", "rebalancing", ...]
 
@@ -14,7 +15,9 @@ class Upload(Resource):
 class Report(Resource):
     def run_test(test_number: int) -> bool:
         # compile the code with the specified test and run it
-        pass
+        subprocess.run(f"g++ -std=c++11 main.exe -o {session.username}_test{test_number}")
+        result = subprocess.run(f"./{session.username}_test{test_number}.exe")
+        return True if result.returncode is 0 else False
     
     # sends the report from the unit tests back to the front end
     def get(self):
