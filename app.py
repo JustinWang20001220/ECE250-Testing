9d8d5d456c0bf55efde3566d8d9840bcab035598
JamesOu <jamesou@JamesOus-MacBook-Pro.local>
2021-11-07 10:09:25 -0500

    developing

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask_restful import Api
 from flask import Flask
-from flask_cors import CORS
-from flask_session import Session
+# from flask_cors import CORS
+# from flask_session import Session
 from routes import Upload, Report
 
 class FlaskApp:
     def __init__(self):
         self.app = Flask(__name__)
-        CORS(self.app)
-        Session(self.app)
+        # CORS(self.app)
+        # Session(self.app)
         self.api = Api(self.app, prefix="/api/v0.0")
     
     def runserver(self, **kwargs):
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from flask import request, session
+from flask import request
 import subprocess
 
 TESTS = ["push", "rebalancing", ...]
@@ -8,20 +8,28 @@ class Upload(Resource):
     # gets the uploaded binary file and stores it in a folder (could use database later)
     def post(self):
         h_file = request.files["file"]
-        with open(f"./submissions/{session.username}.h", "w", encoding="utf-8") as f:
+        username = request.form["username"]
+        with open(f"./submissions/{username}.h", "w", encoding="utf-8") as f:
             f.write(h_file)
+        return 200
 
 
 class Report(Resource):
-    def run_test(test_number: int) -> bool:
+    def run_test(test_number: int, username: str) -> bool:
+        
+        
         # compile the code with the specified test and run it
-        subprocess.run(f"g++ -std=c++11 main.exe -o {session.username}_test{test_number}")
-        result = subprocess.run(f"./{session.username}_test{test_number}.exe")
+        subprocess.run(f"cp ./submissions/{username}.h ./test_space/Search_tree.h")
+
+        subprocess.run(f"g++ -std=c++11 test{test_number}.cpp -o {username}_test{test_number}")
+        result = subprocess.run(f"./test_space/{username}_test{test_number}.out")
         return True if result.returncode is 0 else False
     
     # sends the report from the unit tests back to the front end
     def get(self):
+        username = request.form["username"]
+        
         results: dict = {}
         for i in range(len(TESTS)):
-            results[TESTS[i]] = self.run_test(i)
+            results[TESTS[i]] = self.run_test(i, username)
         return results, 200
